8c7391f48848a5aa908e4c70e60f4aa5800e52cf
Baemint2 <qofndlwl@gmail.com>
2024-05-16 01:59:06 +0900

    관리자 추가

---
+++ src/main/java/com/mcp/crispy/admin/mapper/AdminMapper.java
@@ -0,0 +1,17 @@
+package com.mcp.crispy.admin.mapper;
+
+import com.mcp.crispy.admin.dto.AdminDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface AdminMapper {
+
+//    void insertAdmin(AdminDto adminDto);
+
+    @Select("SELECT ADMIN_ID, ADMIN_PW FROM ADMIN_T WHERE ADMIN_ID = #{adminId}")
+    AdminDto findByUsername(String username);
+
+}
